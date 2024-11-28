4a663fae6e5792ca348d4b5ce6c790e7ca88c4f2
Ricardo de Souza <ricardosouzatdf@gmail.com>
2024-11-27 22:54:52 -0300

    Adding tests

--- cars/tests.py
+++ cars/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
-# Create your tests here.
+from cars.models import Car
+
+
+
+class CarModelTest(TestCase):
+    def setUp(self):
+        self.car = Car.objects.create(
+            brand="Ford",
+            model="Focus",
+            year=2010,
+            price=30000.00
+        )
+
+    def test_car_creation(self):
+        self.assertEqual(self.car.brand, "Ford")
+        self.assertEqual(self.car.model, "Focus")
+        self.assertEqual(self.car.year, 2010)
+        self.assertEqual(float(self.car.price), 30000.00)
+
+    def test_car_string_representation(self):
+        self.assertEqual(str(self.car), "Ford Focus (2010)")
